da37cf993c52b4b45cd09f813dbec17e6bb3d50d
Demonsleader <115692396+DEMONSLEADER@users.noreply.github.com>
2022-12-01 13:54:03 +0530

    Update Mention_all.py

--- Shikimori/modules/Mention_all.py
+++ Shikimori/modules/Mention_all.py
@@ -30,7 +30,7 @@ from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from NekoRobot import tbot
+from shikimori import tbot
 
 spam_chats = []
 
