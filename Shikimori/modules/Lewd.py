3ddd15cc5d5bf5f65db5e14530977a2b5c82a061
Demonsleader <115692396+DEMONSLEADER@users.noreply.github.com>
2022-12-01 10:52:09 +0530

    Lewd.py

---
+++ Shikimori/modules/Lewd.py
@@ -0,0 +1,212 @@
+LEWD_HANDLER = CommandHandler("lewd", lewd, run_async=True)
+FEED_HANDLER = CommandHandler("feed", feed, run_async=True)
+EROYURI_HANDLER = CommandHandler("eroyuri", eroyuri, run_async=True)
+ERON_HANDLER = CommandHandler("eron", eron, run_async=True)
+CUM_HANDLER = CommandHandler("cum", cum, run_async=True)
+BJGIF_HANDLER = CommandHandler("bjgif", bjgif, run_async=True)
+BJ_HANDLER = CommandHandler("bj", bj, run_async=True)
+NEKONSFW_HANDLER = CommandHandler("nekonsfw", nekonsfw, run_async=True)
+SOLO_HANDLER = CommandHandler("solo", solo, run_async=True)
+KEMONOMIMI_HANDLER = CommandHandler("kemonomimi", kemonomimi, run_async=True)
+AVATARLEWD_HANDLER = CommandHandler("avatarlewd", avatarlewd, run_async=True)
+GASM_HANDLER = CommandHandler("gasm", gasm, run_async=True)
+POKE_HANDLER = CommandHandler("poke", poke, run_async=True)
+ANAL_HANDLER = CommandHandler("anal", anal, run_async=True)
+HENTAI_HANDLER = CommandHandler("hentai", hentai, run_async=True)
+AVATAR_HANDLER = CommandHandler("avatar", avatar, run_async=True)
+EROFEET_HANDLER = CommandHandler("erofeet", erofeet, run_async=True)
+HOLO_HANDLER = CommandHandler("holo", holo, run_async=True)
+TITS_HANDLER = CommandHandler("tits", tits, run_async=True)
+PUSSYGIF_HANDLER = CommandHandler("pussygif", pussygif, run_async=True)
+HOLOERO_HANDLER = CommandHandler("holoero", holoero, run_async=True)
+PUSSY_HANDLER = CommandHandler("pussy", pussy, run_async=True)
+HENTAIGIF_HANDLER = CommandHandler("hentaigif", hentaigif, run_async=True)
+CLASSIC_HANDLER = CommandHandler("classic", classic, run_async=True)
+KUNI_HANDLER = CommandHandler("kuni", kuni, run_async=True)
+LEWD_HANDLER = CommandHandler("lewd", lewd, run_async=True)
+KISS_HANDLER = CommandHandler("kiss", kiss, run_async=True)
+FEMDOM_HANDLER = CommandHandler("femdom", femdom, run_async=True)
+CUDDLE_HANDLER = CommandHandler("cuddle", cuddle, run_async=True)
+HUG_HANDLER = CommandHandler("hug", hug, run_async=True)
+EROK_HANDLER = CommandHandler("erok", erok, run_async=True)
+FOXGIRL_HANDLER = CommandHandler("foxgirl", foxgirl, run_async=True)
+TITSGIF_HANDLER = CommandHandler("titsgif", titsgif, run_async=True)
+ERO_HANDLER = CommandHandler("ero", ero, run_async=True)
+SMUG_HANDLER = CommandHandler("smug", smug, run_async=True)
+BAKA_HANDLER = CommandHandler("baka", baka, run_async=True)
+DVA_HANDLER = CommandHandler("dva", dva, run_async=True)
+
+
+NEKO_PTB.add_handler(ADD_NSFW_HANDLER)
+NEKO_PTB.add_handler(REMOVE_NSFW_HANDLER)
+NEKO_PTB.add_handler(LIST_NSFW_CHATS_HANDLER)
+NEKO_PTB.add_handler(LEWDKEMO_HANDLER)
+NEKO_PTB.add_handler(NEKO_HANDLER)
+NEKO_PTB.add_handler(FEET_HANDLER)
+NEKO_PTB.add_handler(YURI_HANDLER)
+NEKO_PTB.add_handler(TRAP_HANDLER)
+NEKO_PTB.add_handler(FUTANARI_HANDLER)
+NEKO_PTB.add_handler(HOLOLEWD_HANDLER)
+NEKO_PTB.add_handler(SOLOGIF_HANDLER)
+NEKO_PTB.add_handler(CUMGIF_HANDLER)
+NEKO_PTB.add_handler(EROKEMO_HANDLER)
+NEKO_PTB.add_handler(LESBIAN_HANDLER)
+NEKO_PTB.add_handler(WALLPAPER_HANDLER)
+NEKO_PTB.add_handler(LEWDK_HANDLER)
+NEKO_PTB.add_handler(NGIF_HANDLER)
+NEKO_PTB.add_handler(TICKLE_HANDLER)
+NEKO_PTB.add_handler(LEWD_HANDLER)
+NEKO_PTB.add_handler(FEED_HANDLER)
+NEKO_PTB.add_handler(EROYURI_HANDLER)
+NEKO_PTB.add_handler(ERON_HANDLER)
+NEKO_PTB.add_handler(CUM_HANDLER)
+NEKO_PTB.add_handler(BJGIF_HANDLER)
+NEKO_PTB.add_handler(BJ_HANDLER)
+NEKO_PTB.add_handler(NEKONSFW_HANDLER)
+NEKO_PTB.add_handler(SOLO_HANDLER)
+NEKO_PTB.add_handler(KEMONOMIMI_HANDLER)
+NEKO_PTB.add_handler(AVATARLEWD_HANDLER)
+NEKO_PTB.add_handler(GASM_HANDLER)
+NEKO_PTB.add_handler(POKE_HANDLER)
+NEKO_PTB.add_handler(ANAL_HANDLER)
+NEKO_PTB.add_handler(HENTAI_HANDLER)
+NEKO_PTB.add_handler(AVATAR_HANDLER)
+NEKO_PTB.add_handler(EROFEET_HANDLER)
+NEKO_PTB.add_handler(HOLO_HANDLER)
+NEKO_PTB.add_handler(TITS_HANDLER)
+NEKO_PTB.add_handler(PUSSYGIF_HANDLER)
+NEKO_PTB.add_handler(HOLOERO_HANDLER)
+NEKO_PTB.add_handler(PUSSY_HANDLER)
+NEKO_PTB.add_handler(HENTAIGIF_HANDLER)
+NEKO_PTB.add_handler(CLASSIC_HANDLER)
+NEKO_PTB.add_handler(KUNI_HANDLER)
+NEKO_PTB.add_handler(LEWD_HANDLER)
+NEKO_PTB.add_handler(KISS_HANDLER)
+NEKO_PTB.add_handler(FEMDOM_HANDLER)
+NEKO_PTB.add_handler(CUDDLE_HANDLER)
+NEKO_PTB.add_handler(HUG_HANDLER)
+NEKO_PTB.add_handler(EROK_HANDLER)
+NEKO_PTB.add_handler(FOXGIRL_HANDLER)
+NEKO_PTB.add_handler(TITSGIF_HANDLER)
+NEKO_PTB.add_handler(ERO_HANDLER)
+NEKO_PTB.add_handler(SMUG_HANDLER)
+NEKO_PTB.add_handler(BAKA_HANDLER)
+NEKO_PTB.add_handler(DVA_HANDLER)
+
+__handlers__ = [
+    ADD_NSFW_HANDLER,
+    REMOVE_NSFW_HANDLER,
+    LIST_NSFW_CHATS_HANDLER,
+    NEKO_HANDLER,
+    FEET_HANDLER,
+    YURI_HANDLER,
+    TRAP_HANDLER,
+    FUTANARI_HANDLER,
+    HOLOLEWD_HANDLER,
+    SOLOGIF_HANDLER,
+    CUMGIF_HANDLER,
+    EROKEMO_HANDLER,
+    LESBIAN_HANDLER,
+    WALLPAPER_HANDLER,
+    LEWDK_HANDLER,
+    NGIF_HANDLER,
+    TICKLE_HANDLER,
+    LEWD_HANDLER,
+    FEED_HANDLER,
+    EROYURI_HANDLER,
+    ERON_HANDLER,
+    CUM_HANDLER,
+    BJGIF_HANDLER,
+    BJ_HANDLER,
+    NEKONSFW_HANDLER,
+    SOLO_HANDLER,
+    KEMONOMIMI_HANDLER,
+    AVATARLEWD_HANDLER,
+    GASM_HANDLER,
+    POKE_HANDLER,
+    ANAL_HANDLER,
+    HENTAI_HANDLER,
+    AVATAR_HANDLER,
+    EROFEET_HANDLER,
+    HOLO_HANDLER,
+    TITS_HANDLER,
+    PUSSYGIF_HANDLER,
+    HOLOERO_HANDLER,
+    PUSSY_HANDLER,
+    HENTAIGIF_HANDLER,
+    CLASSIC_HANDLER,
+    KUNI_HANDLER,
+    LEWD_HANDLER,
+    KISS_HANDLER,
+    FEMDOM_HANDLER,
+    LEWDKEMO_HANDLER,
+    CUDDLE_HANDLER,
+    EROK_HANDLER,
+    FOXGIRL_HANDLER,
+    TITSGIF_HANDLER,
+    ERO_HANDLER,
+    SMUG_HANDLER,
+    BAKA_HANDLER,
+    DVA_HANDLER,
+    HUG_HANDLER,
+]
+
+__help__ = """
+*ENABLE AND DISABLE* :
+    
+/addnsfw `*:*Enable NSFW mode
+/rmnsfw `*:*Disable NSFW mode
+ 
+*Commands* `*:*  
+   • `/neko`*:*Sends Random SFW Neko source Images.
+   • `/feet`*:*Sends Random Anime Feet Images.
+   • `/yuri`*:*Sends Random Yuri source Images.
+   • `/trap`*:*Sends Random Trap source Images.
+   • `/futanari`*:*Sends Random Futanari source Images.
+   • `/hololewd`*:*Sends Random Holo Lewds.
+   • `/lewdkemo`*:*Sends Random Kemo Lewds.
+   • `/sologif`*:*Sends Random Solo GIFs.
+   • `/cumgif`*:*Sends Random Cum GIFs.
+   • `/erokemo`*:*Sends Random Ero-Kemo Images.
+   • `/lesbian`*:*Sends Random Les Source Images.
+   • `/lewdk`*:*Sends Random Kitsune Lewds.
+   • `/ngif`*:*Sends Random Neko GIFs.
+   • `/tickle`*:*Sends Random Tickle GIFs.
+   • `/lewd`*:*Sends Random Lewds.
+   • `/feed`*:*Sends Random Feeding GIFs.
+   • `/eroyuri`*:*Sends Random Ero-Yuri source Images.
+   • `/eron`*:*Sends Random Ero-Neko source Images.
+   • `/cum`*:*Sends Random Cum Images.
+   • `/bjgif`*:*Sends Random Blow Job GIFs.
+   • `/bj`*:*Sends Random Blow Job source Images.
+   • `/nekonsfw`*:*Sends Random NSFW Neko source Images.
+   • `/solo`*:*Sends Random NSFW Neko GIFs.
+   • `/kemonomimi`*:*Sends Random KemonoMimi source Images.
+   • `/avatarlewd`*:*Sends Random Avater Lewd Stickers.
+   • `/gasm`*:*Sends Random Orgasm Stickers.
+   • `/poke`*:*Sends Random Poke GIFs.
+   • `/anal`*:*Sends Random Anal GIFs.
+   • `/hentai`*:*Sends Random Hentai source Images.
+   • `/avatar`*:*Sends Random Avatar Stickers.
+   • `/erofeet`*:*Sends Random Ero-Feet source Images.
+   • `/holo`*:*Sends Random Holo source Images.
+   • `/tits`*:*Sends Random Tits source Images.
+   • `/pussygif`*:*Sends Random Pussy GIFs.
+   • `/holoero`*:* Sends Random Ero-Holo source Images.
+   • `/pussy`*:* Sends Random Pussy source Images.
+   • `/hentaigif`*:* Sends Random Hentai GIFs.
+   • `/classic`*:* Sends Random Classic Hentai GIFs.
+   • `/kuni`*:* Sends Random Pussy Lick GIFs.
+   • `/kiss`*:* Sends Random Kissing GIFs.
+   • `/femdom`*:* Sends Random Femdom source Images.
+   • `/cuddle`*:* Sends Random Cuddle GIFs.
+   • `/erok`*:* Sends Random Ero-Kitsune source Images.
+   • `/foxgirl`*:* Sends Random FoxGirl source Images.
+   • `/titsgif`*:* Sends Random Tits GIFs.
+   • `/ero`*:* Sends Random Ero source Images.
+   • `/smug`*:* Sends Random Smug GIFs.
+   • `/baka`*:* Sends Random Baka Shout GIFs.
+   • `/dva`*:* Sends Random D.VA source Images.
+"""
+
+__mod_name__ = "NSFW"
