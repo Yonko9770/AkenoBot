33355af962a7ccec8a42cd7c0896a180624dd220
Kakashi <87579616+YeahKakashi@users.noreply.github.com>
2023-02-05 14:15:37 +0530

    Update alive.py

--- Shikimori/modules/alive.py
+++ Shikimori/modules/alive.py
@@ -62,9 +62,9 @@ def awake(update: Update, context: CallbackContext):
 
 ⚪ I'm alive & Working Fine. Any doubt
 
-⚪ My Owner : <a href="https://t.me/{OWNER_USERNAME}">{OWNER_USERNAME}</a></b>
+⚪ My Owner : <a href="https://t.me/{OWNER_USERNAME}">{OWNER_USERNAME}</a>
 
-⚪ My One and Only devloper is @YeahKakashi.
+⚪ My One and Only devloper : <a href="https://t.me/YeahKakashi"></a></b>
     """
     if NETWORK:
         TEXT = TEXT + f'\n⚪ <b>I am Powered by : <a href="https://t.me/{NETWORK_USERNAME}">{NETWORK}</a>\n\n' + 'Thanks For Adding Me Here ❤️</b>'
