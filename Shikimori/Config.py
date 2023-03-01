37c7c305a8aa7800bc1fb902837b466c2152f007
Yonko9770 <121147878+Yonko9770@users.noreply.github.com>
2023-03-01 11:06:11 +0530

    Update Config.py

--- Shikimori/Config.py
+++ Shikimori/Config.py
@@ -39,7 +39,7 @@ class Config(object):
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
     DEV_USERS = get_user_list("elevated_users.json", "devs")
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    LOLIS = get_user_list("elevated_users.json", "supports")
+    DEMONS = get_user_list("elevated_users.json", "supports")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     TIGERS = get_user_list("elevated_users.json", "tigers")
     WOLVES = get_user_list("elevated_users.json", "whitelists")
