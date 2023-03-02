7568dc60abdd702a20f9ba8c1180c29abf745cc8
Yonko9770 <121147878+Yonko9770@users.noreply.github.com>
2023-03-02 11:35:34 +0530

    Update gtranslator.py

--- Shikimori/modules/gtranslator.py
+++ Shikimori/modules/gtranslator.py
@@ -35,47 +35,16 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 from gpytranslate import Translator
-from telegram.ext import CallbackContext
-from telegram import (
-    Message,
-    Update,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton,
-)
-from Shikimori import dispatcher, pbot
+from Shikimori import pbot
 from pyrogram import filters, enums
-from Shikimori.modules.disable import DisableAbleCommandHandler
-
-
-__help__ = """ 
-Use this module to translate stuff!
-*Commands:*
-❂ `/tl` (or /tr): as a reply to a message, translates it to English.
-❂ `/tl` <lang>: translates to <lang>
-eg: `/tl` ja: translates to Japanese.
-❂ `/tl` <source>//<dest>: translates from <source> to <lang>.
-eg:  `/tl` ja//en: translates from Japanese to English.
-❂ `/langs`: get a list of supported languages for translation.
-
-I can convert text to voice and voice to text..
-❂ `/tts` <lang code>*:* Reply to any message to get text to speech output
-*Language Codes*
-`af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
-et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
-id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
-ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
-sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
-vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
-"""
-
-__mod_name__ = "Translator"
+from pyrogram.types import Message
 
 
 trans = Translator()
 
 
 @pbot.on_message(filters.command(["tl", "tr"]))
-async def translate(_, message: Message) -> None:
+async def translate(_, message: Message):
     reply_msg = message.reply_to_message
     if not reply_msg:
         await message.reply_text("Reply to a message to translate it!")
@@ -101,26 +70,4 @@ async def translate(_, message: Message) -> None:
         f"<code>{translation.text}</code>"
     )
 
-    await message.reply_text(reply, parse_mode= enums.ParseMode.HTML)
-
-
-def languages(update: Update, context: CallbackContext) -> None:
-    update.effective_message.reply_text(
-        "Click on the button below to see the list of supported language codes.",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="Language codes",
-                        url="https://telegra.ph/Lang-Codes-03-19-3",
-                    ),
-                ],
-            ],
-            disable_web_page_preview=True,
-        ),
-    )
-
-
-LANG_HANDLER = DisableAbleCommandHandler("langs", languages, run_async=True)
-
-dispatcher.add_handler(LANG_HANDLER)
+    await message.reply_text(reply, parse_mode=enums.ParseMode.HTML)
